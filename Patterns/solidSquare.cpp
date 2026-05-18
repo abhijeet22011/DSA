e24caef5c4f4c571cb28d2bdf4e41cb18e8c7c8d
abhijeet22011 <abhijeet22011@gmail.com>
2026-05-18 13:43:57 +0530

    add hollow square pattern implementation

---
+++ Patterns/solidSquare.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void printHollowSquare(int n) {
+    // Implement the pattern printing logic here
+    for (int row = 1; row <= n; row++) {
+      if (row == 1 || row == n) {
+        for (int col = 1; col <= n; col++) {
+          cout << "*";
+        }
+
+      } else {
+        cout << "*";
+        for (int col = 1; col <= n - 2; col++) {
+          cout << " ";
+        }
+        cout << "*";
+      }
+      cout << "\n";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the square: ";
+    cin >> n;
+    printHollowSquare(n);
+    return 0;
+}  
