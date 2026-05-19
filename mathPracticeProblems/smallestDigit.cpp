ddfb18d65bb94e094efb0edd87ee5530558d59d0
Abhijeet Chaurasiya <abhijeet22011@gmail.com>
2026-05-19 17:24:54 +0530

    add implementation to find the smallest digit in a number

---
+++ mathPracticeProblems/smallestDigit.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int smallestDigit(int num) {
+        int smallest = INT_MAX ; // Initialize smallest to the maximum possible integer value
+        while(num!=0){
+           int lastdigit = num % 10;
+           if(smallest>lastdigit) smallest=lastdigit;
+           num = num / 10;
+        }
+        return smallest;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Smallest digit: " << smallestDigit(num) << endl;
+    return 0;
+}
