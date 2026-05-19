976a599fb0b682ebeb00f43576e9d8b9240e7315
abhijeet22011 <abhijeet22011@gmail.com>
2026-05-19 10:45:46 +0530

    add solid rectangle pattern implementation

---
+++ patterns/solidRectangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printSolidRectangle(int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n, m;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    cout << "Enter the number of columns: ";
+    cin >> m;
+    printSolidRectangle(n, m);
+    return 0;
+}
