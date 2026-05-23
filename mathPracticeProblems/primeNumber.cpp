20fc85785da8b47df6cdc7d5f12a04d9c9ede755
Abhijeet Chaurasiya <abhijeet22011@gmail.com>
2026-05-23 11:43:10 +0530

    add implementation for prime number checker

---
+++ mathPracticeProblems/primeNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+string isPrime(int num) {
+        if(num==1) return "No";
+        if(num==2) return "Yes";
+        for(int i=2;i<num;i++){
+            if(num%i==0) return "No";
+        }
+        return "Yes";
+}
+
+int main(){
+    int num;
+    cout << "Enter a number :";
+    cin >> num;
+    cout << isPrime(num);
+
+    return 0;
+}
