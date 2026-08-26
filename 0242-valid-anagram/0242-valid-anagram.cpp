bc9bfe6e9b20c3e1868796c7e4945711c91adf70
Abhijeet Chaurasiya <abhijeet22011@gmail.com>
2026-08-26 21:15:09 +0530

    Time: 3 ms (45.05%), Space: 9.8 MB (26.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
-            return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        int arr[128] = {0};
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            arr[ch]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t[i];
+            arr[ch]--;
+        }
+
+        for(int i = 0; i<128;i++){
+            if(arr[i] != 0) return false;
+        }
+
+        return true;
     }
 };
